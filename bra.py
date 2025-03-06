9dfb886652f3e7d5f97a14db3b84a0c94f8b4f45
TheRatDefence-Mac <theratdefence@gmail.com>
2025-03-06 15:24:00 +1100

    working on 0.0.1

--- bra.py
+++ bra.py
@@ -5,6 +5,16 @@ class Vec3:
         self.z = z
     def __str__(self):
         return "(%.2f, %.2f, %.2f)" % (self.x, self.y, self.z)
+
+class Vec4:
+    def __init__(self, x, y, z, w):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+
+    def __str__(self):
+        return "(%.2f, %.2f, %.2f)" % (self.x, self.y, self.z)
 class Vec2:
     def __init__(self, x, y):
         self.x = x
@@ -50,3 +60,50 @@ class Mat3:
                             0, 0, 0,
                             0, 0, 0])
 
+class Mat4:
+    def __init__(self, values):
+        self.values = values
+
+    def __getitem__(self, key):
+        return self.values[key[0] * 4 + key[1]]
+
+    def __mul__(self, other):
+        if type(other) is Mat4:
+            return Mat4([
+                self[(0, 0)] * other[(0, 0)] + self[(0, 1)] * other[(1, 0)] + self[(0, 2)] * other[(2, 0)],
+                self[(0, 0)] * other[(0, 1)] + self[(0, 1)] * other[(1, 1)] + self[(0, 2)] * other[(2, 1)],
+                self[(0, 0)] * other[(0, 2)] + self[(0, 1)] * other[(1, 2)] + self[(0, 2)] * other[(2, 2)],
+                self[(0, 0)] * other[(0, 3)] + self[(0, 1)] * other[(1, 3)] + self[(0, 2)] * other[(2, 3)],
+
+                self[(1, 0)] * other[(0, 0)] + self[(1, 1)] * other[(1, 0)] + self[(1, 2)] * other[(2, 0)],
+                self[(1, 0)] * other[(0, 1)] + self[(1, 1)] * other[(1, 1)] + self[(1, 2)] * other[(2, 1)],
+                self[(1, 0)] * other[(0, 2)] + self[(1, 1)] * other[(1, 2)] + self[(1, 2)] * other[(2, 2)],
+                self[(1, 0)] * other[(0, 3)] + self[(1, 1)] * other[(1, 3)] + self[(1, 2)] * other[(2, 3)],
+
+                self[(2, 0)] * other[(0, 0)] + self[(2, 1)] * other[(1, 0)] + self[(2, 2)] * other[(2, 0)],
+                self[(2, 0)] * other[(0, 1)] + self[(2, 1)] * other[(1, 1)] + self[(2, 2)] * other[(2, 1)],
+                self[(2, 0)] * other[(0, 2)] + self[(2, 1)] * other[(1, 2)] + self[(2, 2)] * other[(2, 2)],
+                self[(2, 0)] * other[(0, 3)] + self[(2, 1)] * other[(1, 3)] + self[(2, 2)] * other[(2, 3)],
+
+                self[(3, 0)] * other[(0, 0)] + self[(3, 1)] * other[(1, 0)] + self[(3, 2)] * other[(2, 0)],
+                self[(3, 0)] * other[(0, 1)] + self[(3, 1)] * other[(1, 1)] + self[(3, 2)] * other[(2, 1)],
+                self[(3, 0)] * other[(0, 2)] + self[(3, 1)] * other[(1, 2)] + self[(3, 2)] * other[(2, 2)],
+                self[(3, 0)] * other[(0, 3)] + self[(3, 1)] * other[(1, 3)] + self[(3, 2)] * other[(2, 3)],
+            ])
+        elif type(other) is Vec4:
+            return Vec4(
+                self[(0, 0)] * other.x + self[(0, 1)] * other.y + self[(0, 2)] * other.z + self[(0, 3)] * other.w,
+                self[(1, 0)] * other.x + self[(1, 1)] * other.y + self[(1, 2)] * other.z + self[(1, 3)] * other.w,
+                self[(2, 0)] * other.x + self[(2, 1)] * other.y + self[(2, 2)] * other.z + self[(2, 3)] * other.w,
+                self[(3, 0)] * other.x + self[(3, 1)] * other.y + self[(3, 2)] * other.z + self[(3, 3)] * other.w,
+            )
+
+    def __str__(self):
+        return f"[%i, %i, %i, %i\n %i, %i, %i, %i\n %i, %i, %i, %i\n %i, %i, %i, %i]"
+
+    @staticmethod
+    def zero():
+        return Mat3([       0, 0, 0, 0,
+                            0, 0, 0, 0,
+                            0, 0, 0, 0,
+                            0, 0, 0, 0])
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import turtle
 import time
+from math import radians
+
 import numpy as np
 import random as r
 from bra import *
 
-def turt_init():
+def turtle_init():
     t = turtle.Turtle()
     t.hideturtle()
     t.speed(0)
@@ -15,9 +17,9 @@ def turt_init():
     return t
 #3D Face
 class Face:
-    def __init__(self, points):
+    def __init__(self, p):
         #Asign verts
-        self.verts = points
+        self.verts = p
         self.colour = (r.randint(0, 255), r.randint(0, 255), r.randint(0, 255))
 
     def Translate(self, X, Y, Z):
@@ -27,7 +29,9 @@ class Face:
             v[1, 0] + X, v[1, 1] + Y, v[1, 2] + Z,
             v[2, 0] + X, v[2, 1] + Y, v[2, 2] + Z
         ])
-    def Rotate(self, axis, angle):
+
+    def Rotate(self, axis, radians):
+        angle = np.radians(radians)
         if axis == 'x':
             R = Mat3([
                 1,          0,          0,
@@ -42,11 +46,11 @@ class Face:
             ])
         elif axis == 'z':
             R = Mat3([
-                np.arccos(angle), np.arcsin(angle) * -1, 0,
-                np.arcsin(angle), np.arccos(angle), 0,
-                0, 0, 1
+                np.cos(angle),   np.sin(angle) * -1,  0,
+                np.sin(angle),   np.cos(angle),       0,
+                0,                  0,                      1
             ])
-            print(f"cos angle = {np.arccos(angle)}, sin anle = {np.arcsin(angle)}")
+
         A = R * Vec3(self.verts[0,0],self.verts[0,1],self.verts[0,2],)
         B = R * Vec3(self.verts[1,0],self.verts[1,1],self.verts[1,2],)
         C = R * Vec3(self.verts[2,0],self.verts[2,1],self.verts[2,2],)
@@ -58,49 +62,125 @@ class Face:
 
 #3D Camera
 class Camera:
-    def __init__(self, focal_length, x, y, z):
-        self.focal_length = focal_length
+    def __init__(self, x, y, z, near, far, fov):
         self.x = x
         self.y = y
         self.z = z
+        self.near = near
+        self.far = far
+        self.fov = fov
+
+    def Translate(self, X, Y, Z):
+        v = self
+        v.x += X
+        v.y += Y
+        v.z += Z
+
+
+    def Rotate(self, axis, radians):
+        angle = np.radians(radians)
+        if axis == 'x':
+            R = Mat3([
+                1,          0,          0,
+                0,          np.cos(angle),  -1 * np.sin(angle),
+                0,          np.sin(angle),  np.cos(angle)
+            ])
+        elif axis == 'y':
+            R = Mat3([
+                np.cos(angle), 0, np.sin(angle),
+                0, 1, 0,
+                np.sin(angle) * -1, 0, np.cos(angle)
+            ])
+        elif axis == 'z':
+            R = Mat3([
+                np.cos(angle),   np.sin(angle) * -1,  0,
+                np.sin(angle),   np.cos(angle),       0,
+                0,                  0,                      1
+            ])
+
+        verts = R * Vec3(self.x,self.y,self.x)
+        self.x,self.y,self.z = verts.x,verts.y,verts.z
+
 #2D Triangle
-class Triange:
+class Triangle:
     def __init__(self, face, A: Vec2, B: Vec2, C: Vec2):
         #Create Turtle and colour
-        self.turtle = turt_init()
+        self.turtle = turtle_init()
 
         #Asign verts
         self.verts = [A,B,C]
+        print(f"Triangle created with\nA = [{A.x},{A.y}]\nB = [{B.x},{B.y}]\nC = [{C.x},{C.y}]")
 
 def calculate_vert(vert, camera):
-    wX, wY, wZ = vert
-    f = camera.focal_length
+    near = camera.near
+    far = camera.far
+    fov = camera.fov
+    x, y, z = vert
+    Rfov = 1 / np.tan(fov * 0.5 * 3.1459 / 180 )
+    screenX, screenY = turtle.screensize()
+    aspect_ratio = screenX/screenY
 
-    #calculate relative
-    wX = wX - camera.x
-    wY = wY - camera.y
-    wZ = wZ - (camera.z - f)
+    S = 1 / (np.tan((fov/2)*(3.1415926/180)))
 
-    #calculate screenspace y
-    Yangle = np.arctan(wY/wZ)
-    sY = f * np.tan(Yangle)
+    print(f"Far = {far}, near = {near} \n {far/(far - near)}")
 
-    #calculate screenspace x
-    Xangle = np.arctan(wX / wZ)
-    sX = f * np.tan(Xangle)
+    matProj = Mat4([
+        S, 0,0,0,
+        0, S, 0,0,
+        0,0,(far * -1) / (far - near),-1,
+        0,0,((-1 * far) * near) / (far - near),0
+    ])
+    print(f"1{matProj[0,0]},{matProj[0,1]},{matProj[0,2]},{matProj[0,3]}\n"
+          f"2{matProj[1,0]},{matProj[1,1]},{matProj[1,2]},{matProj[1,3]}\n"
+          f"3{matProj[2,0]},{matProj[2,1]},{matProj[2,2]},{matProj[2,3]}\n"
+          f"4{matProj[3,0]},{matProj[3,1]},{matProj[3,2]},{matProj[3,3]}\n")
 
-    return Vec2(sX,sY)
+    projFace = multmat4(Vec3(x,z,y), matProj)
+
+    print(f"2D Y = {projFace.y * 0.5 * screenY}")
+
+    return Vec2(projFace.x * 0.5 * screenX,projFace.y * 0.5 * screenY)
+
+def multmat4(i, m):
+    o = Vec3(0,0,0)
+    o.x = i.x * m[0,0] + i.y * m[1,0] + i.z * m[2,0] + m[3,0]
+    o.y = i.x * m[0,1] + i.y * m[1,1] + i.z * m[2,1] + m[3,1]
+    o.z = i.x * m[0,2] + i.y * m[1,2] + i.z * m[2,2] + m[3,2]
+    w =   i.x * m[0,3] + i.y * m[1,3] + i.z * m[2,3] + m[3,3]
+    if (w != 0):
+        o.x /= w
+        o.y /= w
+        o.z /= w
+        print("Dividing")
+
+    return (o)
 
 def flatten_face(face: Face, camera: Camera): #Returns a Triangle which is sceenspace equivalent of the face
     points = []
     v = face.verts
-    #Calculate each vert's screenspace equivalent
+    vmat = [0,0,0,
+         0,0,0,
+         0,0,0,]
+    n = camera.near
+    f = camera.far
 
+    #for i in range(3):
+        #y = verts[i,2]
+        #vmat[i * 3 + 0] = (verts[i,0] * n)
+        #vmat[i * 3 + 2] = (verts[i,2] * n)
+        #vmat[i * 3 + 1] = (verts[i,1] * ((f + n) * verts[i,1] - (f * n)))
+        #print(f"{i} succeeded")
+
+    #v = Mat3([vmat[0], vmat[1], vmat[2],
+              #vmat[3], vmat[4], vmat[5],
+              #vmat[6], vmat[7], vmat[8],])
+
+    #Calculate each vert's screenspace equivalent
     points = [calculate_vert((v[0,0],v[0,1],v[0,2]), camera),
               calculate_vert((v[1,0],v[1,1],v[1,2]), camera),
               calculate_vert((v[2,0],v[2,1],v[2,2]), camera)]
 
-    return Triange(face, points[0], points[1], points[2])
+    return Triangle(face, points[0], points[1], points[2])
 
 def draw_triangle(tri, face):
     #Assign vars
@@ -115,9 +195,9 @@ def draw_triangle(tri, face):
     t.teleport(verts[-1].x,verts[-1].y)
     i = 0
     for vert in verts:
-        wX = int(face.verts[i,0])
-        wY = int(face.verts[i, 1])
-        wZ = int(face.verts[i, 2])
+        wX = face.verts[i,0]
+        wY = face.verts[i,1]
+        wZ = face.verts[i,2]
 
 
         t.goto(vert.x,vert.y)
@@ -125,7 +205,7 @@ def draw_triangle(tri, face):
         colour = face.colour
         t.color(0, 0, 0)
 
-        t.write(f"{wX}, {wY}, {wZ}")
+        t.write(f"{round(wX)}, {round(wY)}, {round(wZ)}")
 
         t.color(colour)
 
@@ -137,52 +217,84 @@ def draw_triangle(tri, face):
 
 
 points = Mat3([
-        0, 0, 5,
-        0, 1000, 5 ,
-        1000, 1000, 5
+        0, 10, 0,
+        0, 10, 50,
+        50, 10, 50
     ])
 
+
+
 face = Face(points)
-camera = Camera(2.5005, 0,0,0)
+print(f"World space points = \n{face.verts}")
+camera = Camera(0,0,0, 1, 2000, 90)
+print(f"Camera at {camera.x},{camera.y},{camera.z}")
 
 frames = []
-
-print('#' * 5)
-
-matrix = Mat3([
-    1,2,3,
-    4,5,6,
-    7,8,9,
-])
-
-print(matrix * matrix)
 
 fps = 60
 i = int(0)
 dance = int(0)
 
-face.Translate(0,0,10)
 
-face.Rotate('z', 90)
+print(f"World space points after translation = \n{face.verts}")
+
+
+def on_motion(event):
+    global mouse_x, mouse_y
+    mouse_x = event.x - turtle.window_width() / 2
+    mouse_y = -event.y + turtle.window_height() / 2
+def move_face(face, frame, lastframe):
+    x,y = frame
+    lx, ly = lastframe
+    dx,dy = x - lx, y - ly
+    face.Translate(dx * 0.01, dy * 0.01, 0)
+def rotate_face(face, frame, lastframe):
+    x, y = frame
+    lx, ly = lastframe
+    dx, dy = x - lx, y - ly
+    face.Rotate('z', dx)
+def rotate_camera(camera, frame, lastframe):
+    x, y = frame
+    lx, ly = lastframe
+    dx, dy = x - lx, y - ly
+    face.Rotate('z', dx)
+def move_camera(camera, frame, lastframe):
+    x,y = frame
+    lx, ly = lastframe
+    dx,dy = x - lx, y - ly
+    camera.Translate(dx * 0.01, dy * 0.01, 0)
+
+
+mouse_x, mouse_y = 0,0
+cords = []
+turtle.getcanvas().bind("<Motion>", on_motion)
+
 
 running = True
 while running:
     i += 1
 
-    #face.Rotate('y', i * 0.00001)
+    cords.append((mouse_x, mouse_y))
+    if i > 2:
+        cords.remove(cords[0])
+        #move_face(face, cords[-1], cords[-2])
 
     #Stores, draws, clears and removes frames
     frames.append(flatten_face(face, camera))
-
-
-    if i > 3:
-        frames[2].turtle.clear()
-        draw_triangle(frames[3], face)
+    if i > 2:
+        frames[1].turtle.clear()
+        draw_triangle(frames[2], face)
+        turtle.turtles().remove(frames[1].turtle)
         frames.pop(1)
+
 
 
     turtle.update()
     if i % 60 == 0:
+        #face.Rotate('y', 1)
+        #camera.Translate(100,0,0)
+        #print(camera.x)
+        #camera.Rotate('y', 20)
         end_time = time.time()
         try:
             fps = round((fps + (60 / (end_time - start_time))) / 2, 1)
